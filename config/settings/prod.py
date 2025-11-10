f873708c77f798c7dd40625d4c535e7142ed11a0
Andry Nyaina <andriniaina.2am@gmail.com>
2025-11-10 06:38:39 +0300

    feat: setup Django CleanStarter with environment variables and multi-settings structure
    - Added base/dev/prod settings separation
    - Configured .env for secret management using python-decouple
    - Added .gitignore for safe version control
    - Verified server startup (OK)

---
+++ config/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['mydomain.com', 'www.mydomain.com']
+
+# Exemple : base PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
